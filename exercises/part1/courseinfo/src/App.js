c953bbf0cdfe78d3f4cdbbd907aa0a80e49f965e
ruloalv <ruloalv@gmail.com>
2023-06-16 15:50:55 -0300

    course info first commit

---
+++ exercises/part1/courseinfo/src/App.js
@@ -0,0 +1,41 @@
+const Header = (props) =>{
+  return <h1>{props.name}</h1>
+}
+
+const Part1 = (props) =>{
+  return <p>
+    {props.text} {props.ex}
+  </p>
+}
+
+const Content = (props) =>{
+  return <>
+    <Part1 text={props.text} ex={props.exercises} />
+  </>
+}
+
+const Total = (props) =>{
+  return <p>{props.text} {props.total}
+  </p>
+}
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Header name={course} />
+      <Content text={part1} exercises={exercises1} />
+      <Content text={part2} exercises={exercises2} />
+      <Content text={part3} exercises={exercises3} />
+      <Total text='Number of exercises' total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+}
+
+export default App;
